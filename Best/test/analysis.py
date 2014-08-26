ff05896e482a7317751ea330f9d327a47a24951b
Junghwan John Goh <jhgoh@cern.ch>
2014-08-26 19:06:32 +0200

    systematic error calculator and generic interface for the analysis result

---
+++ Best/test/analysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from ROOT import *
+
+def analyze(fileName):
+    f = TFile(fileName)
+    #h = f.Get("SEvt/hnvertex")
+    h = f.Get("SEvt/hMt")
+    h.Sumw2()
+    h.Scale(1./h.Integral())
+    #h = f.Get("SEvt/hnjets")
+    result = []
+    for i in range(1, h.GetNbinsX()+1):
+        x  = h.GetBinCenter(i)
+        xLo = h.GetBinLowEdge(i)
+        xHi = xLo + h.GetBinWidth(i)
+        y  = h.GetBinContent(i)
+        ey = h.GetBinError(i)
+        result.append((x, xLo, xHi, y, ey, ey))
+    return result
